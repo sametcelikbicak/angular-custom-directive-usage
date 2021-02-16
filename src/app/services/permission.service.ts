5fed4ac671b79093c01ca97d04e83b7b0ce7739a
sametcelikbicak <sametcelikbicak@outlook.com>
2021-02-16 21:36:06 +0300

    Add directive usage

--- src/app/services/permission.service.ts
+++ src/app/services/permission.service.ts
@@ -2,7 +2,18 @@ import { Injectable } from "@angular/core";
 
 @Injectable()
 export class PermissionService {
-  private validPermissions: ["read", "write", "remove"];
+  private validPermissions = ["read", "write", "remove"];
+
+  public setUserPermission(permission: string | string[]): void {
+    switch (typeof permission) {
+      case "string":
+        this.validPermissions = [permission];
+        break;
+      case "object":
+        this.validPermissions = permission.map(p => p);
+        break;
+    }
+  }
 
   public verify(permission: string): boolean {
     return this.validPermissions.some(
