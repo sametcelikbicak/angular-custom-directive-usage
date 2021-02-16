d9b5dbaed68772b2f7ef3317358c9fdbaa1d92b0
sametcelikbicak <sametcelikbicak@outlook.com>
2021-02-16 10:22:33 +0300

    Remove hello component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+  title = `Angular ${VERSION.major} Custom Directive`;
 }
